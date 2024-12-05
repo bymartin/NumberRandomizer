530c5e7c3e10a62e308bd2b31bc3f990c564b655
Barry Martin <bymartin@users.noreply.github.com>
2024-12-05 09:41:50 -0600

    Add the functionality to generate random number and display in the UI

--- NumberRandomizer/ContentView.swift
+++ NumberRandomizer/ContentView.swift
@@ -8,12 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var randomNum: Int?
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(randomNum != nil ? "\(randomNum!)" : "?")
+                .font(.system(size: 30))
+                .padding()
+                
+            Button {
+                randomNum = Int.random(in: 1...100)
+            } label: {
+                Text("Generate Random Number")
+            }
         }
         .padding()
     }
