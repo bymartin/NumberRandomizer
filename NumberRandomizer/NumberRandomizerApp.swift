d57f233f37d06d249963b87ce8a296c54c39be7e
Barry Martin <bymartin@users.noreply.github.com>
2024-12-04 21:29:57 -0600

    Initial Commit

---
+++ NumberRandomizer/NumberRandomizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumberRandomizerApp.swift
+//  NumberRandomizer
+//
+//  Created by Barry Martin on 12/4/24.
+//
+
+import SwiftUI
+
+@main
+struct NumberRandomizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NumberRandomizerTests/NumberRandomizerTests.swift
@@ -0,0 +1,17 @@
+//
+//  NumberRandomizerTests.swift
+//  NumberRandomizerTests
+//
+//  Created by Barry Martin on 12/4/24.
+//
+
+import Testing
+@testable import NumberRandomizer
+
+struct NumberRandomizerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
